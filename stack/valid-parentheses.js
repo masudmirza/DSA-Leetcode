14128330b5486caa7a161fd5dba020a77234e6bc
Masud Mirzazada <masudmirzazada@gmail.com>
2025-06-03 19:12:16 +0400

    valid-parentheses

---
+++ stack/valid-parentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (const char of s) {
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+            console.log('stack ', stack);
+            
+        } else {
+            if (stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
+
+const input = '({([])})';
+console.log(isValid(input));
