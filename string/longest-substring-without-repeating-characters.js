6a7207cffe9a9ba255a9d50339b847ad2e073e2e
Masud Mirzazada <masudmirzazada@gmail.com>
2025-08-15 12:29:09 +0400

    optimize: longest substring without repeating characters

--- string/longest-substring-without-repeating-characters.js
+++ string/longest-substring-without-repeating-characters.js
@@ -2,28 +2,42 @@
  * @param {string} s
  * @return {number}
  */
+// var lengthOfLongestSubstring = function(s) {
+//   if (s.length === 0) return 0;
+
+//   let current = "";
+//   let bestLen = 0;
+
+//   for (let i = 0; i < s.length; i++) {
+//     const char = s[i];
+//     const dupIdx = current.indexOf(char);
+
+//     if (dupIdx === -1) {
+//       current += char;
+//     } else {
+//       current = current.slice(dupIdx + 1) + char;
+//     }
+
+//     if (current.length > bestLen) bestLen = current.length;
+//   }
+
+//   return bestLen;
+// };
+
 var lengthOfLongestSubstring = function(s) {
-  if (s.length === 0) return 0;
+  let left = 0, best = 0;
+  const seen = new Set();
 
-  let current = "";
-  let bestLen = 0;
-
-  for (let i = 0; i < s.length; i++) {
-    const char = s[i];
-    const dupIdx = current.indexOf(char);
-
-    if (dupIdx === -1) {
-      current += char;
-    } else {
-      current = current.slice(dupIdx + 1) + char;
+  for (let right = 0; right < s.length; right++) {
+    while (seen.has(s[right])) {
+      seen.delete(s[left]);
+      left++;
     }
-
-    if (current.length > bestLen) bestLen = current.length;
+    seen.add(s[right]);
+    best = Math.max(best, right - left + 1);
   }
-
-  return bestLen;
+  return best;
 };
-
 
 const input = 'dvdf';
 console.log(lengthOfLongestSubstring(input));
