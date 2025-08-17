c1cfd6b56f9a911347ce3946e82801f483768500
Masud Mirzazada <masudmirzazada@gmail.com>
2025-08-17 15:48:24 +0400

    reverse bits

---
+++ bit-manipulation/reverse-bits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var reverseBits = function(n) {
+  let res = 0 >>> 0;
+  
+  for (let i = 0; i < 32; i++) {
+    res = (res << 1) | (n & 1);
+    n >>>= 1;
+  }
+
+  return res >>> 0;
+};
